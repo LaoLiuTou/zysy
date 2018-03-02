1dc284965b0e9fd295abfb00e85d59969b40298c
TongTong <369259353@qq.com>
2018-03-02 11:24:25 +0800

    0226

---
+++ WebRoot/pages/js/main/keydown.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    //按键切换
+    $('.dom').bind('keydown', function (e) {
+
+        var key = e.which;
+        switch (key) {
+            case 38: //上
+
+
+                break;
+            case 40: //下
+
+                break;
+            case 37: //左(會導致輸入時無法使用左右移)
+                e.preventDefault();
+                var keyIndex=$(this).attr('keyIndex');
+                $('.dom[keyIndex="'+Number(Number(keyIndex)-1)+'"]').focus();
+                break;
+            case 39: //右(會導致輸入時無法使用左右移)
+                e.preventDefault();
+                var keyIndex=$(this).attr('keyIndex');
+                $('.dom[keyIndex="'+Number(Number(keyIndex)+1)+'"]').focus();
+                break;
+            case 13: //右(會導致輸入時無法使用左右移)
+                e.preventDefault();
+                var keyIndex=$(this).attr('keyIndex');
+                $('.dom[keyIndex="'+Number(Number(keyIndex)+1)+'"]').focus();
+                break;
+            default:
+                return;
+        }
+
+    });
+});
